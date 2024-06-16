e16d411ad56ffdd92b650b82cb554de84d39ef5b
tmpstpdwn <tmpstpdwn@proton.me>
2024-06-16 22:19:37 +0530

    pygame installation added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 ### IMPORTS ###
+import importlib
 
+### Check if pygame is installed ###
+try:
+    importlib.import_module('pygame')
+    print("pygame is already installed.")
+except ImportError:
+    print("pygame is not installed. Installing...")
+    import subprocess
+    subprocess.check_call(['pip', 'install', 'pygame'])
+    print("pygame has been successfully installed.")
+
+### Import FloopyBirdy after pygame installation ###
 from floopybirdy import FloopyBirdy
 
 ### MAIN ###
-
 if __name__ == "__main__":
     floopyInstance = FloopyBirdy()
     floopyInstance.run()
